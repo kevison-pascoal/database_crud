c4febbbe165ecb03d979e8c7757678f7f3eedd33
kevison.developer@gmail.com <kevison.developer@gmail.com>
2022-05-13 19:17:41 -0300

    modify

--- nodejs_server/test.js
+++
@@ -1,8 +0,0 @@
-//Variáveis de desenvolvimento não podem ser utilizadas em produção.
-//Isto é apenas um TESTE.
-if(process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
-
-require('dotenv').config();
-console.log(process.env.NODE_ENV);
