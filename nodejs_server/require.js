40da8814a5c58327e9ae555e775f8e685ad0c320
kevison.developer@gmail.com <kevison.developer@gmail.com>
2022-05-14 12:24:20 -0300

    require

--- nodejs_server/require.js
+++ nodejs_server/require.js
@@ -2,12 +2,23 @@ console.clear();
 const chalk = require('chalk');
 console.log(chalk.red('Progress Bar'));
 const ProgressBar = require('progress');
+
 const bar = new ProgressBar(':bar', {total: 100});
 const timer = setInterval(() => {
   bar.tick();
   if(bar.complete) {
     clearInterval(timer);
     console.log(chalk.green('Complete progress'));
-    console.log('');
+    console.log();
+    console.log(chalk.red('Second BarProgress'));
   }
-}, 50);
+}, 1);
+
+const barProgress = new ProgressBar(':bar', time = 1000);
+const interval = setInterval(() => {
+  barProgress.tick();
+  if (barProgress.complete) {
+    clearInterval(interval);
+    console.log(chalk.green('Complete The Second Progress'));
+  }
+}, 1);
