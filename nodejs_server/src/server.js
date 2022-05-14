3d764c4f82a580d4ba2de819f69d7a388ba13fd0
kevison.developer@gmail.com <kevison.developer@gmail.com>
2022-05-14 15:04:25 -0300

    exports

--- nodejs_server/src/server.js
+++ nodejs_server/src/server.js
@@ -1,14 +1,19 @@
 const http = require('http');
-//import https from "http";
-
+const items = require('../skills/export');
+const { dataBase, moreData } = require('../skills/export');
 const host = '127.0.0.1';
 const port = 3000;
+
+console.log('DataBase is: %o', dataBase);
+console.log('moreBase is: %o', moreData);
 
 const app = http.createServer((request, response) => {
   response.statusCode = 200;
   response.setHeader('Content-Type', 'text/plain');
   response.write("My first server with nodejs :)\n");
-  response.end(`Hello World in port ${port} and host ${host}!\n`);
+  response.write(`Hello World in port ${port} and host ${host}!\n`);
+  response.write(`DataBase:\n${JSON.stringify(dataBase)}`)
+  response.end(`\nMoreBase:\n${JSON.stringify(moreData)}`);
 })
 
 app.listen(port, () => {
