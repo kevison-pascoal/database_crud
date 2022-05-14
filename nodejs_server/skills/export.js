5e9eb86be62bcb9c7c8be86f0b23084015d695ef
kevison.developer@gmail.com <kevison.developer@gmail.com>
2022-05-14 15:05:06 -0300

    skills_export

---
+++ nodejs_server/skills/export.js
@@ -0,0 +1,14 @@
+const dataBase = {
+  name: 'Kevison',
+  idade: 19,
+  profissional: true,
+  local: 'Recife | Pernambuco',
+};
+
+const moreData = {
+  sub: 'Strange',
+  power: 'HiperInteligence', 
+}
+
+exports.dataBase = dataBase;
+exports.moreData = moreData;
