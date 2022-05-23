8fc2fba8d46ef7149461f049a38e14607723e9e8
kevison.developer@gmail.com <kevison.developer@gmail.com>
2022-05-23 16:40:15 -0300

    added eventLoop :package:

---
+++ nodejs_server/process/eventLoop.js
@@ -0,0 +1,40 @@
+const bar = () => console.log('bar');
+const baz = () => console.log('baz');
+const fo = () => {
+  console.log('foo');
+  bar();
+  baz();
+}
+fo();
+/* 
+foo()
+console.log('foo')
+bar()
+console.log('bar')
+baz()
+console.log('baz');
+*/
+
+const foo = () => {
+  console.log('foo');
+  setTimeout(bar, 0);
+  baz();
+}
+foo();
+/* 
+foo()
+console.log('foo')
+setTimeout()
+baz()
+console.log('baz')
+bar()
+console.log('bar')
+*/
+
+const foO = () => {
+  console.log('foo');
+  setTimeout(bar, 0);
+  new Promise((resolve, reject) => resolve('should be right after baz, before bar')).then(resolve => console.log(resolve));
+  baz();
+}
+foO();
